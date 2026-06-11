707cbe482f22e5e48494cd16c71d3caf2eb7fa0e
zero <guzhang0@gmail.com>
2026-06-11 10:14:53 +0800

    etc.

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,165 @@
-#include <iostream>
-
-#define NOMINMAX
-#include <Windows.h>
+#include <windows.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
+#include <wrl.h>
+#include <iostream>
+#include <vector>
 
+using namespace Microsoft::WRL;
+
+const UINT FrameCount = 2;
+HWND g_hwnd = nullptr;
+ComPtr<ID3D12Device> g_device;
+ComPtr<ID3D12CommandQueue> g_commandQueue;
+ComPtr<IDXGISwapChain3> g_swapChain;
+ComPtr<ID3D12DescriptorHeap> g_rtvHeap;
+ComPtr<ID3D12Resource> g_renderTargets[FrameCount];
+ComPtr<ID3D12CommandAllocator> g_commandAllocator;
+ComPtr<ID3D12GraphicsCommandList> g_commandList;
+ComPtr<ID3D12Fence> g_fence;
+UINT64 g_fenceValue = 0;
+HANDLE g_fenceEvent;
+UINT g_frameIndex;
+UINT g_rtvDescriptorSize;
+
+void InitD3D12() {
+    UINT dxgiFactoryFlags = 0;
 #ifdef _DEBUG
-#include <d3d12sdklayers.h>
-#include <dxgidebug.h>
+    ComPtr<ID3D12Debug> debugController;
+    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
+        debugController->EnableDebugLayer();
+        dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+    }
 #endif
 
-int main(int, char**){
-    std::cout << "Hello, from learn_d3d12!" << std::endl;
+    ComPtr<IDXGIFactory4> factory;
+    CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory));
 
-    POINT pt;
-    GetCursorPos(&pt);
-    std::cout << pt.x << " " << pt.y << std::endl;
+    if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&g_device)))) {
+        std::cout << "D3D12 Device Creation Failed!" << std::endl;
+        return;
+    }
+
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    g_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&g_commandQueue));
+
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.BufferCount = FrameCount;
+    swapChainDesc.Width = 800;
+    swapChainDesc.Height = 600;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count = 1;
+
+    ComPtr<IDXGISwapChain1> swapChain;
+    factory->CreateSwapChainForHwnd(g_commandQueue.Get(), g_hwnd, &swapChainDesc, nullptr, nullptr, &swapChain);
+    swapChain.As(&g_swapChain);
+    g_frameIndex = g_swapChain->GetCurrentBackBufferIndex();
+
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+    rtvHeapDesc.NumDescriptors = FrameCount;
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    g_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&g_rtvHeap));
+
+    g_rtvDescriptorSize = g_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle(g_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+    for (UINT n = 0; n < FrameCount; n++) {
+        g_swapChain->GetBuffer(n, IID_PPV_ARGS(&g_renderTargets[n]));
+        g_device->CreateRenderTargetView(g_renderTargets[n].Get(), nullptr, rtvHandle);
+        rtvHandle.ptr += g_rtvDescriptorSize;
+    }
+
+    g_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&g_commandAllocator));
+
+    g_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, g_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&g_commandList));
+    g_commandList->Close();
+
+    g_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&g_fence));
+    g_fenceValue = 1;
+    g_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+}
+
+void WaitForPreviousFrame() {
+    const UINT64 fence = g_fenceValue;
+    g_commandQueue->Signal(g_fence.Get(), fence);
+    g_fenceValue++;
+
+    if (g_fence->GetCompletedValue() < fence) {
+        g_fence->SetEventOnCompletion(fence, g_fenceEvent);
+        WaitForSingleObject(g_fenceEvent, INFINITE);
+    }
+
+    g_frameIndex = g_swapChain->GetCurrentBackBufferIndex();
+}
+
+void Render() {
+    g_commandAllocator->Reset();
+    g_commandList->Reset(g_commandAllocator.Get(), nullptr);
+
+    D3D12_RESOURCE_BARRIER barrier = {};
+    barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    barrier.Transition.pResource = g_renderTargets[g_frameIndex].Get();
+    barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+    barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+    g_commandList->ResourceBarrier(1, &barrier);
+
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle(g_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+    rtvHandle.ptr += g_frameIndex * g_rtvDescriptorSize;
+
+    const float clearColor[] = { 0.4f, 0.6f, 0.9f, 1.0f };
+    g_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+    barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+    g_commandList->ResourceBarrier(1, &barrier);
+
+    g_commandList->Close();
+
+    ID3D12CommandList* ppCommandLists[] = { g_commandList.Get() };
+    g_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+    g_swapChain->Present(1, 0);
+
+    WaitForPreviousFrame();
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+int main(int argc, char** argv)
+{
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    WNDCLASSEX windowClass = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WindowProc, 0, 0, hInstance, nullptr, nullptr, nullptr, nullptr, L"DX12WindowClass", nullptr };
+    RegisterClassEx(&windowClass);
+
+    g_hwnd = CreateWindow(windowClass.lpszClassName, L"DX12 Hello World", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, nullptr, nullptr, hInstance, nullptr);
+
+    InitD3D12();
+
+    ShowWindow(g_hwnd, true);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        } else {
+            Render();
+        }
+    }
+
+    return 0;
 }
